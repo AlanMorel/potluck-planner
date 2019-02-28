454c9a368a86c05876ccc826840de1e4d738c757
Alan Morel <amorel@dotdash.com>
2019-02-28 09:42:28 -0500

    Clean up of original files

---
+++ src/components/PotluckPlanner.vue
@@ -0,0 +1,29 @@
+<template>
+    <h1>Potluck Planner</h1>
+</template>
+
+<script>
+    export default {
+        name: "PotluckPlanner"
+    }
+</script>
+
+<style lang="scss">
+    h3 {
+      margin: 40px 0 0;
+    }
+
+    ul {
+      list-style-type: none;
+      padding: 0;
+    }
+
+    li {
+      display: inline-block;
+      margin: 0 10px;
+    }
+
+    a {
+      color: #42b983;
+    }
+</style>
--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Vue from "vue";
+import Router from "vue-router";
+import PotluckPlanner from "./components/PotluckPlanner.vue";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    }
-  ]
-})
+    mode: "history",
+    base: process.env.BASE_URL,
+    routes: [
+        {
+          path: "/",
+          name: "PotluckPlanner",
+          component: PotluckPlanner
+        }
+    ]
+});
