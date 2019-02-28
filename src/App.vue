4e8a9f0417281754a80a849793512cfb43c04af4
Alan Morel <amorel@dotdash.com>
2019-02-28 09:56:25 -0500

    Starting on initial create potluck form

--- src/App.vue
+++ src/App.vue
@@ -9,12 +9,17 @@
 </template>
 
 <style lang="scss">
+    body {
+        margin: 0;
+    }
+
     #app {
         font-family: 'Avenir', Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
+        background-color: #F2F5F8;
     }
 
     #nav {
--- src/components/PotluckPlanner.vue
+++ src/components/PotluckPlanner.vue
@@ -1,14 +1,46 @@
 <template>
-    <h1>Potluck Planner</h1>
+    <section>
+        <h1>Potluck Planner</h1>
+        <form @submit.prevent="createNewPotluck">
+            <div>
+                <label for="event">Event Name</label>
+                <input type="text" name="event" v-model="event">
+            </div>
+            <div>
+                <label for="host">Host Name</label>
+                <input type="text" name="host" v-model="host">
+            </div>
+            <div>
+                <label for="people">Number of People</label>
+                <input type="number" name="people" v-model="people">
+            </div>
+            <input type="submit" value="Create New Potluck">
+        </form>
+    </section>
 </template>
 
 <script>
     export default {
-        name: "PotluckPlanner"
+        name: "PotluckPlanner",
+        data() {
+            return {
+                event: "",
+                host: "",
+                people: 1
+            }
+        },
+        methods: {
+            createNewPotluck() {
+                console.log("create new potluck!");
+            }
+        }
     }
 </script>
 
 <style lang="scss">
+    label {
+        display: block;
+    }
     h3 {
       margin: 40px 0 0;
     }
